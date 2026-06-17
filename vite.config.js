b7ba5e9d06a99ba0c32352d2f294507dc91d0584
MAK <denbor140@gmail.com>
2026-06-17 23:48:42 +0300

    upd vite.config and partial

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig(({ command }) => {
   return {
     base: command === 'build' ? '/Devhorizon/' : '/',
     root: 'src',
+    publicDir: '../public',
     build: {
       sourcemap: true,
       outDir: '../dist',
